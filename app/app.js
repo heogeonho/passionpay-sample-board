53d240a9427ea82d992337aeeac540238294f97e
heogeonho <geonh1016@gmail.com>
2023-10-29 03:12:09 +0900

    feat: app.js에 쿠키, 세션 설정

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ const nunjucks = require('nunjucks');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const dotenv = require('dotenv');
 
 const connect = require('./schemas');
 const pageRouter = require('./routes/page');
@@ -12,6 +13,7 @@ const passportConfig = require('./passport');
 //const usersRouter = require('./routes/users');
 //const commentsRouter = require('./routes/comments');
 
+dotenv.config();
 const app = express();
 passportConfig(); // 패스포트 설정
 app.set('port', process.env.PORT || 3002);
@@ -26,7 +28,16 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    reave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    }
+}))
 app.use(passport.initialize());
 app.use(passport.session());
 
