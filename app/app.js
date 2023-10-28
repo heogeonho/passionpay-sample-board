89141fa07d00d172d629c297501746eb32d5af34
heogeonho <geonh1016@gmail.com>
2023-10-29 04:51:14 +0900

    fix: GET /auth/kakao 라우터 없음 오류 해결
    auth 라우터 url 오타 수정

--- app/app.js
+++ app/app.js
@@ -42,7 +42,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', pageRouter);
-app.use('auth/', authRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
