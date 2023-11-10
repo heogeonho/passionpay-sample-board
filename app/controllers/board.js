08474da2abe0c0e4e5bae4911e47276e23537ea6
heogeonho <geonh1016@gmail.com>
2023-11-10 23:31:19 +0900

    feat: 삭제 과정을 위한 컨트롤러 코드 추가
    로그인된 유저의 document의 subdocument를 post id로 찾아 삭제하는 과정

--- app/controllers/board.js
+++ app/controllers/board.js
@@ -8,8 +8,8 @@ exports.createPost = async (req, res) => {
             content: content,
         };
         if (req.isAuthenticated()) {
-            const snsId = req.user.id;
-            const user = await User.findById({ _id: snsId }).exec();
+            const id = req.user.id;
+            const user = await User.findById({ _id: id }).exec();
             if (user) {
                 user.board.push(newPost);
                 await user.save();
@@ -21,5 +21,20 @@ exports.createPost = async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).json({ success: false, message: '게시물 생성 중 오류가 발생했습니다.' });
+    }
+};
+
+exports.deletePost = async (req, res) => {
+    const postId = req.params.postId;
+
+    try {
+        const id = req.user.id;
+        const user = await User.findById({ _id: id }).exec();
+        user.board.remove({_id: postId});
+        await user.save();
+        res.json({success: true, message:'게시물이 삭제되었습니다.'});
+    } catch (err) {
+        res.json({success: false, message:'실패!'});
+        console.error(err);
     }
 };
