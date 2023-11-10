eb86f23a0000d41cb18651219537d51887bf3ee4
heogeonho <geonh1016@gmail.com>
2023-11-10 23:28:54 +0900

    feat: 요청 라우팅 코드 추가

--- app/routes/board.js
+++ app/routes/board.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const boardController = require('../controllers/board');
 
 router.post('/', boardController.createPost);
+router.delete('/delete-post/:postId', boardController.deletePost);
 
 module.exports = router;
